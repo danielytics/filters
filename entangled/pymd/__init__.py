5902b47af4ebdac70e760352fcfce8378b49f162
Dan Kersten <danielytics@users.noreply.github.com>
2021-07-12 03:11:56 +0100

    Add id attribute to generated markup for pymd formatter

--- entangled/pymd/__init__.py
+++ entangled/pymd/__init__.py
@@ -3,21 +3,29 @@
 __all__ = ["format", "validator"]
 # ~\~ end
 # ~\~ begin <<lit/pymd.md|pymd>>[1]
-def format(source, language, css_class, options, md, classes=None, id_value='', **kwargs):
+def format(source, language, css_class, options, md, classes=None, id_value='', attrs=None, **kwargs):
     patched_source = source \
         .replace("<", "&lt;") \
         .replace(">", "&gt;")
     code_block = "<pre><code class={}>{}</code></pre>".format(language, patched_source)
-    ann = "<div class=\"lp-fragment\"><div class=\"lp-ref\">{}</div>{}</div>"
+    ann = "<div class=\"lp-fragment\"><div class=\"lp-ref\" id=\"lp-{}\">{}</div>{}</div>"
     if "file" in options:
-        name = "«file://{}»".format(options["file"])
-        return ann.format(name, code_block)
+        id = options["file"]
+        name = "«file://{}»".format(id)
+        return ann.format(id, name, code_block)
     elif "id" in options:
-        name = "«{}»".format(options["id"])
-        return ann.format(name, code_block)
+        id = options["id"]
+        name = "«{}»".format(id)
+        return ann.format(id, name, code_block)
     return code_block
 # ~\~ end
 # ~\~ begin <<lit/pymd.md|pymd>>[2]
-def validator(language, options):
+def validator(language, inputs, options, attrs, md):
+    if 'id' in inputs: 
+        options['id'] = inputs['id']
+    elif 'file' in inputs:
+        options['file'] = inputs['file']
+    else:
+        return False
     return True
 # ~\~ end
